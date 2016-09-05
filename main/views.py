6edf7f59651d9990f8583fc3689b63854fa1c3e2
He Huang <hjhhh3000@gmail.com>
2016-09-05 13:44:21 +0800

    make an simple home page

--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse('Hello, world!')
+    template = loader.get_template('index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
