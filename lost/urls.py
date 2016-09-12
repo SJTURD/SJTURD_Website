6e14dc5a761a2595e6ed085b00fb049cbc8f3bdb
caidandaren <476594558@qq.com>
2016-09-13 03:48:14 +0800

    render the default lost list

--- lost/urls.py
+++ lost/urls.py
@@ -3,4 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^items$', views.items, name='found_list'),
+    url(r'^api/getLostItems', views.get_lost_items, name='get_lost_items'),
+    url(r'^match$', views.match, name='match'),
+    url(r'^matchResult$', views.match_result, name='match_result'),
 ]
--- lost/views.py
+++ lost/views.py
@@ -1,3 +1,46 @@
+from django.core import serializers
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.conf import settings
 
-# Create your views here.
+from .models import Lost
+
+
+def items(request):
+    context = {
+        'MEDIA_URL': settings.MEDIA_URL
+    }
+
+    return render(request, 'lost/items.html', context)
+
+def match_found_items(request):
+    ITEMS_PER_PAGE = 10
+
+    data = []
+
+    if not request.is_ajax():
+        return HttpResponse(serializers.serialize('json', data), content_type="application/json")
+
+    params = request.GET
+
+    page = int(params['page'])
+    category = int(params['category'])
+    location = int(params['location'])
+
+    data = lost.objects.all().filter(paired=False).order_by('-date')
+    if category > 0:
+        data = data.filter(category=category)
+    if location > 0:
+        data = data.filter(location=location)
+
+    if len(data) == 0:
+        return HttpResponse(serializers.serialize('json', data), content_type="application/json")
+
+    if not page * ITEMS_PER_PAGE - ITEMS_PER_PAGE < len(data) <= page * ITEMS_PER_PAGE:
+        page = (len(data) - 1) / ITEMS_PER_PAGE + 1
+
+    data = data[page * ITEMS_PER_PAGE - ITEMS_PER_PAGE:page * ITEMS_PER_PAGE]
+
+    return HttpResponse(serializers.serialize('json', data), content_type="application/json")
+
+def match_result(request):
