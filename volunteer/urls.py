1634322019a526ec6b49326cf28944a89ba8cf1d
Hunter-Lin <Linkin-HearT@sjtu.edu.cn>
2016-09-13 22:14:02 +0800

    vol structual setup. Not yet runnable

--- volunteer/urls.py
+++ volunteer/urls.py
@@ -3,4 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	url(r'^login/$', views.login_view, name = "vol login view"),
+	url(r'^index/$', views.index, name = "vol index"),
+	url(r'^upload/$', views.upload_view, name = "vol upload view"),
+	# url(r'^retrieve$', views.retrieve_view, name = "vol retrieve view"),
+	url(r'^api/login', views.login, name = "vol login"),
+	url(r'^api/upload', views.upload, name = "vol upload"),
+	# url(r'^api/retrieve', views.retrieve, name = "vol retrieve"),
 ]
