6a6db1f566aa1f81c05a361637cb2bdbcd8f0eb0
riceroll <584455235@qq.com>
2016-09-29 16:14:35 +0800

    Volunteer Login

--- SJTURD_Website/settings.py
+++ SJTURD_Website/settings.py
@@ -41,13 +41,14 @@ INSTALLED_APPS = [
     'lost.apps.LostConfig',
     'found.apps.FoundConfig',
     'volunteer.apps.VolunteerConfig',
+    'database'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
