8579b61875877db5f923b0341b48df5d18e11bab
caidandaren <476594558@qq.com>
2016-10-02 01:29:12 +0800

    Search Updated

--- lost/urls.py
+++ lost/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^items$', views.items, name='found_list'),
     url(r'^api/getLostItems', views.get_lost_items, name='get_lost_items'),
     url(r'^newLost$', views.new_lost, name='new_lost'),
-    url(r'^newLost/foundList$', views.found_list, name='found_list'),
+    url(r'^newLost/foundList$', views.search, name='found_list'),
     url(r'^uploadLostItem', views.upload_lost_item, name='upload_lost_item'),
     url(r'^uploadResult', views.upload_result, name='upload_result')
 ]
